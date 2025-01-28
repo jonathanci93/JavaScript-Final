85326c25b2b03f9ff61a9f8ce1962c6af7122c3c
Jonathan Carles <jonacarles@gmail.com>
2025-01-28 16:01:26 -0300

    modificacion fetch

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
 const contenedorTarjetas = document.getElementById("productos-container");
 
-// Función para obtener productos desde el archivo JSON
-async function obtenerProductos() {
-  try {
-    const response = await fetch('/suplementos.json');
-    if (!response.ok) {
-      throw new Error('Error al cargar los productos');
-    }
-    const productos = await response.json();
-    crearTarjetasProductosInicio(productos);
-  } catch (error) {
-    console.error(error);
-  }
+//Fetch y promesa asincronica
+const obtenerProductos = async() => {
+  const response = await fetch('/suplementos.json');
+  const productos = await response.json();
+  crearTarjetasProductosInicio(productos);
 }
+
 
 // Creamos las tarjetas del JSON
 function crearTarjetasProductosInicio(productos) {
